bfb49e82deedf76432900b33a099192fa64f9498
Marek Gogol <9mike66@gmail.com>
2020-10-18 11:57:29 +0200

    [bug #2] - ion-toggle infinity loop on ionChange event, if :checked property has been changed by ionChange method.

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -11,18 +11,47 @@
           <ion-title size="large">Tab 1</ion-title>
         </ion-toolbar>
       </ion-header>
-    
-      <ExploreContainer name="Tab 1 page" />
+
+      <h2>Infinity ionChange loop</h2>
+      <ion-toggle :checked="checked" @ionChange="onChangeBuggy"></ion-toggle>
+      <p>checked: {{ checked ? 'yes' : 'no' }}</p>
+      <p>
+        <strong>description:</strong>
+        When you press ion-toggle, state in "checked" property will be immidiately updated. What is okay.
+        <br>
+        <br>
+        But if you change this state programmicaly by this.checked = !this.checked, ionChange will trigger as well. But it should not. Or should it?</p>
     </ion-content>
   </ion-page>
 </template>
 
-<script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+<script>
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonToggle } from '@ionic/vue';
 
 export default  {
   name: 'Tab1',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonToggle },
+
+  data(){
+    return {
+      checked : true,
+      immidiatelyChecked : true,
+    }
+  },
+
+  methods: {
+    onChangeBuggy(e){
+      if ( this.timeout ){
+        clearTimeout(this.timeout);
+      }
+
+      this.checked = e.target.checked;
+
+      //We want turn off checkbox automatically after 1 second
+      this.timeout = setTimeout(() => {
+        this.checked = !e.target.checked;
+      }, 1000);
+    },
+  }
 }
 </script>
